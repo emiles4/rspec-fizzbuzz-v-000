4c344e70459c53968b661d76d312712a9fbcbb0f
Emily Miles <emiles4@gmail.com>
2018-06-28 04:34:06 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -8,7 +8,7 @@ def fizzbuzz(int)
       "Fizz" # Go fizz
       elsif int % 5 == 0
       "Buzz"
-    elsif int % 5 && % 3 == 0
+    elsif (int % 5 ==0) && (int % 3 == 0)
       "FizzBuzz"
     else
       "nil"
