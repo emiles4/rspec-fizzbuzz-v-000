bcf6c86a922574df9913fcc8f7c3edb341bdee61
Emily Miles <emiles4@gmail.com>
2018-06-28 04:26:32 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,7 +2,7 @@
 # See README.md for instructions on how to do this
 require_relative './spec_helper.rb'
 require_relative '../fizzbuzz.rb'
-ruby
+
 
 def fizzbuzz(int)
     if int % 3 == 0 # if the number int is divisible by 3
