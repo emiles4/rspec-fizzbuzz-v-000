e6c3ab67ace407680936a21e0864c83fb1c5f6ef
Emily Miles <emiles4@gmail.com>
2018-06-28 04:41:27 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -4,12 +4,12 @@
 
 
 def fizzbuzz(int)
-    if int % 3 == 0 # if the number int is divisible by 3
+  if int % 5 == 0 && int % 3 ==0
+  "FizzBuzz"
+  elsif int % 3 == 0 # if the number int is divisible by 3
       "Fizz" # Go fizz
     elsif int % 5 == 0
       "Buzz"
-    elsif int % 5 == 0 && int % 3 ==0
-      "FizzBuzz"
       else int
       nil
     end
